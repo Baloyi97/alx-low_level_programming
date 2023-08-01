e77c9fecdb6858279e8f845b20c2f8f505cbdc02
Baloyi97 <vernonbaloyi41@gmail.com>
2023-08-01 12:05:41 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    for (; h; h = h->next)
-    {
-        printf("%d\n", h->n);
-        num++;
-    }
-
-    return num;
+	for (; h; h = h->next)
+	{
+		printf("%d\n", h->n);
+		num++;
+	}
+	return (num);
 }
